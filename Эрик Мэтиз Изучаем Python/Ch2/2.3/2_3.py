f21a53fa06965939e1017359a4b9a48c2cd06a99
Kiberjen <kiberjen@yandex.ru>
2020-08-05 14:38:12 +0300

    тест

---
+++ Эрик Мэтиз Изучаем Python/Ch2/2.3/2_3.py
@@ -0,0 +1,7 @@
+# Личное сообщение. сохраните имя пользователя в переменной и выведите сообщение,
+# предназначеное для конкретного человека. Сообщение должно быть простым - например,
+# "Hello Eric? would you like to learn Python today?"
+
+nameEric = "Eric"
+message = f"Hello {nameEric}? would you like to learn Python today?"
+print(message)
---
+++ Эрик Мэтиз Изучаем Python/Ch2/2.6/2_6.py
@@ -0,0 +1,9 @@
+# Знаменитая цитата 2: повторите упражнение 2.5, но на этот раз сохраните имя автора в переменной famous_person.
+# Затем составьте сообщение его в новой переменной с именем message.
+# Выведите свое собщение.
+#    Albert Einstein once said, "A person who never made
+#    a mistake never tried anything new."
+
+famous_person = "Albert Einstein"
+message = f'\n\t{famous_person} once said, "A person who never made\n\ta mistake never tried anything new."'
+print(message)
