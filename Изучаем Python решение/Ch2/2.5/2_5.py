a3d7d0fed22c742bd2056c3758c5b7977144f70f
Kiberjen <kiberjen@yandex.ru>
2020-08-05 14:33:59 +0300

    тест

--- Изучаем Python решение/Ch2/2.5/2_5.py
+++
@@ -1,6 +0,0 @@
-# Знаменитая цитата: найдите известное высказывание, которое вам понравилось.
-# Выведите текст цитаты с именем автора. Результат должен выглядеть примерно так (включая кавычки)
-#    Albert Einstein once said, "A person who never made
-#    a mistake never tried anything new."
-
-print('\n\tAlbert Einstein once said, "A person who never made\n\ta mistake never tried anything new."')
