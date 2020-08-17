3fb02416bac7f327595b9c566af9f7228dc28512
kiberjen <kiberjen@yandex.ru>
2020-08-17 16:01:29 +0300

    добавил Упражнение 10-13 Зед А Шоу

---
+++ Зед А Шоу Легкий способ выучить Python3/Упражнение 11/ex11.py
@@ -0,0 +1,8 @@
+print("сколько тебе лет?", end='')
+age = input()
+print("каков твой рост?", end='')
+height = input()
+print("сколько ты весишь?", end='')
+weight = input()
+
+print(f"Итак, тебе {age} лет, в тебе {height} см роста и {weight} кг веса.")
